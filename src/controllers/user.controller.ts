991f7f574e14159ec1d4210a293ed3b8655ef2ca
sabari vignesh <sabari@american-technology.net>
2020-02-13 11:42:53 +0530

    added get all messages api

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -4,33 +4,43 @@ import { JoiValidator } from '../config';
 import { UserSchema } from '../schema';
 
 export class UserController {
-  public static route = '/send';
-  public router: Router;
-  private userManager: UserManager;
-  private joiValidator: JoiValidator;
+	public static route = '/send';
+	public router: Router;
+	private userManager: UserManager;
+	private joiValidator: JoiValidator;
 
-  constructor() {
-    this.userManager = new UserManager();
-    this.joiValidator = new JoiValidator();
-    this.router = Router();
-    this.init();
-  }
+	constructor() {
+		this.userManager = new UserManager();
+		this.joiValidator = new JoiValidator();
+		this.router = Router();
+		this.init();
+	}
 
-  public init() {
-    this.router.post('/', this.ping);
-  }
+	public init() {
+		this.router.post('/', this.ping);
+		this.router.get('/all', this.GetAllMessages)
+	}
 
-  public ping = async (request, response, nextFunction) => {
-    try {
-      const result = request.body;
-      const validatedResponse = await this.joiValidator.jsonValidator(
-        new UserSchema().getPingResponse(),
-        result
-      );
-      await this.userManager.publishMessage(validatedResponse);
-      response.send(validatedResponse);
-    } catch (error) {
-      response.status(500).send(error);
-    }
-  };
+	public ping = async (request, response, nextFunction) => {
+		try {
+			const result = request.body;
+			const validatedResponse = await this.joiValidator.jsonValidator(
+				new UserSchema().getPingResponse(),
+				result
+			);
+			await this.userManager.publishMessage(validatedResponse);
+			response.send(validatedResponse);
+		} catch (error) {
+			response.status(500).send(error);
+		}
+	};
+
+	public GetAllMessages = async (request, response, nextFunction) => {
+		try {
+			let result = await this.userManager.GetAllMessages();
+			response.send({ data: result.length ? JSON.parse(result) : [] });
+		} catch (error) {
+			response.status(500).send(error);
+		}
+	}
 }
--- src/managers/user.manager.ts
+++ src/managers/user.manager.ts
@@ -4,114 +4,124 @@ import * as fs from 'fs';
 const fsPromises = fs.promises;
 
 export class UserManager {
-  public publishMessage = async messagePayload => {
-    try {
-      const blockChain = new Blockchain();
-      let newBlock;
-      // let isChainValid;
-      const messageList = await blockChain.getAllBlocks();
-      if (!messageList || !messageList.length) {
-        // isChainValid = true;
-        newBlock = blockChain.createBlock(messagePayload);
-        // messageList = [newBlock];
-      } else {
-        newBlock = blockChain.createBlock(
-          messagePayload,
-          blockChain.getLatestBlock(messageList).hash
-        );
-        // messageList.push(newBlock);
-        // isChainValid = blockChain.isChainValid(messageList);
-        // if (!isChainValid) {
-        // throw { error: 'Invalid Block' };
-        // }
-      }
-      // publish block to all
-      await this.publishBlock('groupchat', { uuid: uuid(), block: newBlock });
-      // save valid chain in file
-      // if (newBlock && isChainValid) {
-      //   await blockChain.saveBlocks(messageList);
-      // }
-      return 'Message Published';
-    } catch (err) {
-      throw err;
-    }
-  };
+	public publishMessage = async messagePayload => {
+		try {
+			const blockChain = new Blockchain();
+			let newBlock;
+			// let isChainValid;
+			const messageList = await blockChain.getAllBlocks();
+			if (!messageList || !messageList.length) {
+				// isChainValid = true;
+				newBlock = blockChain.createBlock(messagePayload);
+				// messageList = [newBlock];
+			} else {
+				newBlock = blockChain.createBlock(
+					messagePayload,
+					blockChain.getLatestBlock(messageList).hash
+				);
+				// messageList.push(newBlock);
+				// isChainValid = blockChain.isChainValid(messageList);
+				// if (!isChainValid) {
+				// throw { error: 'Invalid Block' };
+				// }
+			}
+			// publish block to all
+			await this.publishBlock('groupchat', { uuid: uuid(), block: newBlock });
+			// save valid chain in file
+			// if (newBlock && isChainValid) {
+			//   await blockChain.saveBlocks(messageList);
+			// }
+			return 'Message Published';
+		} catch (err) {
+			throw err;
+		}
+	};
 
-  private publishBlock = async (name, newBlock) => {
-    try {
-      const publisher = new Publish();
-      await publisher.publishBlock(name, newBlock);
-      return true;
-    } catch (err) {
-      throw err;
-    }
-  };
+	private publishBlock = async (name, newBlock) => {
+		try {
+			const publisher = new Publish();
+			await publisher.publishBlock(name, newBlock);
+			return true;
+		} catch (err) {
+			throw err;
+		}
+	};
 
-  public validateAndAckBlock = async block => {
-    try {
-      const blockChain = new Blockchain();
-      let isChainValid = false;
-      const messageList = await blockChain.getAllBlocks();
-      if (!messageList || !messageList.length) {
-        await this.publishBlock('ackChat', { isValid: true, ...block });
-      } else {
-        messageList.push(block.block);
-        isChainValid = blockChain.isChainValid(messageList);
-        await this.publishBlock('ackChat', { isValid: isChainValid, ...block });
-      }
-    } catch (err) {
-      throw err;
-    }
-  };
+	public validateAndAckBlock = async block => {
+		try {
+			const blockChain = new Blockchain();
+			let isChainValid = false;
+			const messageList = await blockChain.getAllBlocks();
+			if (!messageList || !messageList.length) {
+				await this.publishBlock('ackChat', { isValid: true, ...block });
+			} else {
+				messageList.push(block.block);
+				isChainValid = blockChain.isChainValid(messageList);
+				await this.publishBlock('ackChat', { isValid: isChainValid, ...block });
+			}
+		} catch (err) {
+			throw err;
+		}
+	};
 
-  public addBlock = async blockData => {
-    try {
-      const blockChain = new Blockchain();
-      if (blockData.isValid) {
-        let ackBlocks: any = await fsPromises.readFile(
-          __dirname + '/../../ackBlock.txt',
-          'utf8'
-        );
-        ackBlocks = ackBlocks ? JSON.parse(ackBlocks) : {};
-        ackBlocks[blockData.uuid] = parseInt(ackBlocks[blockData.uuid], 10);
-        ackBlocks[blockData.uuid] = ackBlocks[blockData.uuid]
-          ? ackBlocks[blockData.uuid] + 1
-          : 1;
-        if (ackBlocks[blockData.uuid] === 2) {
-          let messageList = await blockChain.getAllBlocks();
-          if (!messageList || !messageList.length) {
-            messageList = [blockData.block];
-          } else {
-            messageList.push(blockData.block);
-          }
-          await blockChain.saveBlocks(messageList);
-        }
-        await fsPromises.writeFile(
-          __dirname + '/../../ackBlock.txt',
-          JSON.stringify(ackBlocks)
-        );
-      }
-      return true;
-    } catch (err) {
-      throw err;
-    }
-  };
+	public addBlock = async blockData => {
+		try {
+			const blockChain = new Blockchain();
+			if (blockData.isValid) {
+				let ackBlocks: any = await fsPromises.readFile(
+					__dirname + '/../../ackBlock.txt',
+					'utf8'
+				);
+				ackBlocks = ackBlocks ? JSON.parse(ackBlocks) : {};
+				ackBlocks[blockData.uuid] = parseInt(ackBlocks[blockData.uuid], 10);
+				ackBlocks[blockData.uuid] = ackBlocks[blockData.uuid]
+					? ackBlocks[blockData.uuid] + 1
+					: 1;
+				if (ackBlocks[blockData.uuid] === 2) {
+					let messageList = await blockChain.getAllBlocks();
+					if (!messageList || !messageList.length) {
+						messageList = [blockData.block];
+					} else {
+						messageList.push(blockData.block);
+					}
+					await blockChain.saveBlocks(messageList);
+				}
+				await fsPromises.writeFile(
+					__dirname + '/../../ackBlock.txt',
+					JSON.stringify(ackBlocks)
+				);
+			}
+			return true;
+		} catch (err) {
+			throw err;
+		}
+	};
 
-  public initiateChain = async () => {
-    try {
-      const blockChain = new Blockchain();
-      const genesisBlock = blockChain.createGenesisBlock();
-      await fsPromises.writeFile(
-        __dirname + '/../../info.txt',
-        JSON.stringify([genesisBlock])
-      );
-      await fsPromises.writeFile(
-        __dirname + '/../../ackBlock.txt',
-        JSON.stringify({})
-      );
-      return true;
-    } catch (err) {
-      throw err;
-    }
-  }
+	public initiateChain = async () => {
+		try {
+			const blockChain = new Blockchain();
+			const genesisBlock = blockChain.createGenesisBlock();
+			await fsPromises.writeFile(
+				__dirname + '/../../info.txt',
+				JSON.stringify([genesisBlock])
+			);
+			await fsPromises.writeFile(
+				__dirname + '/../../ackBlock.txt',
+				JSON.stringify({})
+			);
+			return true;
+		} catch (err) {
+			throw err;
+		}
+	}
+
+	public GetAllMessages = async () => {
+		try {
+			let data = await fsPromises.readFile(
+				__dirname + '/../../info.txt')
+			return data.toString('utf8');
+		} catch (error) {
+			throw error
+		}
+	}
 }
